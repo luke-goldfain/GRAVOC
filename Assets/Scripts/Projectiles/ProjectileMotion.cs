82046d6debdaef7522d916a356aa87b21ac097fb
Luke Uritescu <Luritescu@gmail.com>
2020-04-12 22:21:36 -0500

    Test scene where ball bounces
    I have a script for the movement of the ball, but it doesn't yet disappear after the number of bounces.

---
+++ Assets/Scripts/Projectiles/ProjectileMotion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMotion : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("Just for debugging, adds some velocity during OnEnable")]
+    private Vector3 initialVelocity;
+
+    [SerializeField]
+    private float minVelocity = 10f;
+
+    private Vector3 lastFrameVelocity;
+    private Rigidbody rb;
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = initialVelocity;
+    }
+
+    private void Update()
+    {
+        lastFrameVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bounce(collision.contacts[0].normal);
+    }
+
+    private void Bounce(Vector3 collisionNormal)
+    {
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+
+        Debug.Log("Out Direction: " + direction);
+        rb.velocity = direction * minVelocity;
+    }
+}
