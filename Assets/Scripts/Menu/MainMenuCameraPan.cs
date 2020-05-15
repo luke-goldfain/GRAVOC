cb651f05d99a04454fe5bd56a97fe1903f3c4507
Luke Goldfain <lukegoldfain@gmail.com>
2020-05-14 23:06:18 -0500

    Set up main menu

---
+++ Assets/Scripts/Menu/MainMenuCameraPan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCameraPan : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.down * 0.05f, Space.World);
+    }
+}
